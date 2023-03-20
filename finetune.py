da0e7c8f7a41c3615ea61f5a7b25d34fd608ffcf
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2023-03-20 11:14:06 +0800

    Add support for valid set size 0

--- finetune.py
+++ finetune.py
@@ -33,13 +33,6 @@ TARGET_MODULES = [
     "q_proj",
     "v_proj",
 ]
-# Additional modules to be trained natively
-# additional training on embed and head is good for multilang task
-# FULL_FINETUNE_MODULES = [
-#     "model.embed_tokens", 
-#     "model.lm_head"
-# ]
-FULL_FINETUNE_MODULES = []
 DATA_PATH = "alpaca_data_cleaned.json"
 
 device_map = "auto"
@@ -72,12 +65,6 @@ config = LoraConfig(
 model = get_peft_model(model, config)
 tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos token
 data = load_dataset("json", data_files=DATA_PATH)
-
-train_val = data["train"].train_test_split(
-    test_size=VAL_SET_SIZE, shuffle=True, seed=42
-)
-train_data = train_val["train"]
-val_data = train_val["test"]
 
 
 def generate_prompt(data_point):
@@ -170,8 +157,15 @@ def generate_and_tokenize_prompt(data_point):
     }
 
 
-train_data = train_data.shuffle().map(generate_and_tokenize_prompt)
-val_data = val_data.shuffle().map(generate_and_tokenize_prompt)
+if VAL_SET_SIZE > 0:
+    train_val = data["train"].train_test_split(
+        test_size=VAL_SET_SIZE, shuffle=True, seed=42
+    )
+    train_data = train_val["train"].shuffle().map(generate_and_tokenize_prompt)
+    val_data = train_val["test"].shuffle().map(generate_and_tokenize_prompt)
+else:
+    train_data = data['train'].shuffle().map(generate_and_tokenize_prompt)
+    val_data = None
 
 trainer = transformers.Trainer(
     model=model,
@@ -203,8 +197,8 @@ model.state_dict = (
     lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())
 ).__get__(model, type(model))
 
-if torch.__version__ >= "2":
-    model = torch.compile(model)
+# if torch.__version__ >= "2":
+#     model = torch.compile(model)
 
 trainer.train()
 
