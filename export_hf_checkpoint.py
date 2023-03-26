c4837e3899e8fc7af4b575e0e4f0e43b8784759b
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2023-03-26 17:33:32 +0800

    Update export_hf_checkpoint.py

--- export_hf_checkpoint.py
+++ export_hf_checkpoint.py
@@ -27,15 +27,20 @@ base_model = LlamaForCausalLM.from_pretrained(
 
 first_weight = base_model.model.layers[0].self_attn.q_proj.weight
 first_weight_old = first_weight.clone()
+final_weight = base_model.model.layers[-1].self_attn.q_proj.weight
+final_weight_old = final_weight.clone()
+embed_weight = base_model.model.embed_tokens.weight
+embed_weight_old = base_model.model.embed_tokens.weight.clone()
+head_weight = base_model.lm_head.weight
+head_weight_old = base_model.lm_head.weight.clone()
 
 lora_model = PeftModel.from_pretrained(
     base_model,
-    "tloen/alpaca-lora-7b",
+    "./guanaco",
     device_map={"": "cpu"},
     torch_dtype=torch.float16,
 )
 
-lora_weight = lora_model.base_model.model.model.layers[0].self_attn.q_proj.weight
 
 assert torch.allclose(first_weight_old, first_weight)
 
@@ -51,7 +56,16 @@ lora_model.base_model.model.lm_head.merge_weights = True
 lora_model.train(False)
 
 # did we do anything?
-assert not torch.allclose(first_weight_old, first_weight)
+first_changed = not torch.allclose(first_weight_old, first_weight)
+final_changed = not torch.allclose(final_weight_old, final_weight)
+embed_changed = not torch.allclose(embed_weight_old, embed_weight)
+head_changed = not torch.allclose(head_weight_old, head_weight)
+print(
+    f'first_changed: {first_changed}\n'
+    f'final_changed: {final_changed}\n'
+    f'embed_changed: {embed_changed}\n'
+    f'head_changed : {head_changed}\n'
+)
 
 lora_model_sd = lora_model.state_dict()
 deloreanized_sd = {
@@ -61,5 +75,5 @@ deloreanized_sd = {
 }
 
 LlamaForCausalLM.save_pretrained(
-    base_model, "./hf_ckpt", state_dict=deloreanized_sd, max_shard_size="400MB"
+    base_model, "./hf_ckpt", state_dict=deloreanized_sd, max_shard_size="4GB"
 )
