dd5ba2698debbcccd0606231302e5f1ec189bc8b
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2023-04-05 16:56:43 +0800

    Update for streaming output

--- generate.py
+++ generate.py
@@ -5,6 +5,8 @@ import torch
 from peft import PeftModel
 import transformers
 import gradio as gr
+from utils.callbacks import Iteratorize, Stream
+import tqdm
 
 assert (
     "LlamaTokenizer" in transformers._import_structure["models.llama"]
@@ -50,14 +52,14 @@ def main(
         model = LlamaForCausalLM.from_pretrained(
             base_model,
             load_in_8bit=load_8bit,
-            torch_dtype=torch.float16,
+            torch_dtype=torch.bfloat16,
             device_map="auto",
         )
         if lora_weights:
             model = PeftModel.from_pretrained(
                 model,
                 lora_weights,
-                torch_dtype=torch.float16,
+                torch_dtype=torch.bfloat16,
             )
     elif device == "mps":
         model = LlamaForCausalLM.from_pretrained(
@@ -88,8 +90,8 @@ def main(
     model.config.bos_token_id = 1
     model.config.eos_token_id = 2
 
-    if not load_8bit:
-        model.half()  # seems to fix bugs for some users.
+    # if not load_8bit:
+    #     model.half()  # seems to fix bugs for some users.
 
     model.eval()
     if torch.__version__ >= "2" and sys.platform != "win32":
@@ -102,6 +104,7 @@ def main(
         top_k=45,
         repetition_penalty=1.17,
         max_new_tokens=128,
+        stream_output=False,
         **kwargs,
     ):
         inputs = tokenizer(prompt, return_tensors="pt")
@@ -113,6 +116,45 @@ def main(
             repetition_penalty=repetition_penalty,
             **kwargs,
         )
+        generate_params = {
+            "input_ids": input_ids,
+            "generation_config": generation_config,
+            "return_dict_in_generate": True,
+            "output_scores": True,
+            "max_new_tokens": max_new_tokens,
+        }
+        
+        if stream_output:
+            # Stream the reply 1 token at a time.
+            # This is based on the trick of using 'stopping_criteria' to create an iterator,
+            # from https://github.com/oobabooga/text-generation-webui/blob/ad37f396fc8bcbab90e11ecf17c56c97bfbd4a9c/modules/text_generation.py#L216-L243.
+
+            def generate_with_callback(callback=None, **kwargs):
+                kwargs.setdefault(
+                    "stopping_criteria", transformers.StoppingCriteriaList()
+                )
+                kwargs["stopping_criteria"].append(
+                    Stream(callback_func=callback)
+                )
+                with torch.no_grad():
+                    model.generate(**kwargs)
+
+            def generate_with_streaming(**kwargs):
+                return Iteratorize(
+                    generate_with_callback, kwargs, callback=None
+                )
+
+            with generate_with_streaming(**generate_params) as generator:
+                for output in generator:
+                    # new_tokens = len(output) - len(input_ids[0])
+                    decoded_output = tokenizer.decode(output)
+
+                    if output[-1] in [tokenizer.eos_token_id]:
+                        break
+
+                    yield decoded_output.split("### Response:")[-1].strip()
+            return  # early return for stream_output
+        
         with torch.no_grad():
             generation_output = model.generate(
                 input_ids=input_ids,
@@ -123,7 +165,7 @@ def main(
             )
         s = generation_output.sequences[0]
         output = tokenizer.decode(s)
-        return output.split("### Response:")[1].strip()
+        yield output.split("### Response:")[-1].strip()
     
     with gr.Blocks() as demo:
         with gr.Row() as row:
@@ -145,6 +187,7 @@ def main(
                     top_k=45,
                     repetition_penalty=1.17,
                     max_new_tokens=128,
+                    stream_output=False,
                 ):
                     if inputs.strip() == '':
                         now_prompt = PROMPT_INS.format(instruction)
@@ -152,15 +195,19 @@ def main(
                         now_prompt = PROMPT.format(instruction+'\n', inputs)
                     
                     response = evaluate(
-                        now_prompt, temperature, top_p, top_k, repetition_penalty, max_new_tokens
+                        now_prompt, temperature, top_p, top_k, repetition_penalty, max_new_tokens, stream_output
                     )
-                    return response
+                    if stream_output:
+                        response = tqdm.tqdm(response, unit='token')
+                    for i in response:
+                        i=i
+                        yield i
                 
                 temp = gr.components.Slider(minimum=0, maximum=1, value=0.5, label="Temperature")
                 topp = gr.components.Slider(minimum=0, maximum=1, value=0.75, label="Top p")
-                topk = gr.components.Slider(minimum=0, maximum=100, step=1, value=35, label="Top k")
+                topk = gr.components.Slider(minimum=0, maximum=100, step=1, value=30, label="Top k")
                 repp = gr.components.Slider(
-                    minimum=0, maximum=2, step=0.01, value=1.2, label="Repeat Penalty"
+                    minimum=0, maximum=2, step=0.01, value=1.07, label="Repeat Penalty"
                 )
                 maxt = gr.components.Slider(
                     minimum=1, maximum=2048, step=1, value=512, label="Max tokens"
@@ -168,11 +215,14 @@ def main(
                 maxh = gr.components.Slider(
                     minimum=1, maximum=20, step=1, value=5, label="Max history messages"
                 )
+                stream_output = gr.components.Checkbox(
+                    value=True, label="stream output"
+                )
                 
                 run.click(
                     run_instruction, 
-                    [instruction, inputs, temp, topp, topk, repp, maxt],
-                    answer
+                    [instruction, inputs, temp, topp, topk, repp, maxt, stream_output],
+                    answer,
                 )
             with gr.Column() as col:
                 chat_height = gr.components.Slider(
@@ -203,6 +253,7 @@ def main(
                     repetition_penalty=1.17,
                     max_new_tokens=128,
                     maxh=10,
+                    stream_output=False
                 ):
                     hist = ''
                     for idx, content in enumerate(history):
@@ -215,20 +266,23 @@ def main(
                         assistant = assistant
                         hist += f'User: {user}\nAssistant: {assistant}\n'
                     now_prompt = PROMPT.format(hist, f"User: {history[-1][0]}")
-                    print(now_prompt)
-                    print()
                     
                     bot_message = evaluate(
-                        now_prompt, temperature, top_p, top_k, repetition_penalty, max_new_tokens
+                        now_prompt, temperature, top_p, top_k, repetition_penalty, max_new_tokens, stream_output
                     )
-                    history[-1][1] = bot_message
                     
-                    history = history[-maxh:]
-                    
-                    return history
-
+                    if stream_output:
+                        bot_message = tqdm.tqdm(bot_message, unit='token')
+                    for mes in bot_message:
+                        mes = mes
+                        history[-1][1] = mes
+                        
+                        history = history[-maxh:]
+                        
+                        yield history
+                
                 msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
-                    bot, [chatbot, temp, topp, topk, repp, maxt, maxh], chatbot
+                    bot, [chatbot, temp, topp, topk, repp, maxt, maxh, stream_output], chatbot
                 )
                 clear.click(lambda: None, None, chatbot, queue=False)
         demo.load(None, chat_height, _js="""
@@ -237,7 +291,7 @@ def main(
     document.querySelector('#chatbot .wrap').style.maxHeight = "calc(100% - 45px)"
 }
         """)
-    demo.launch()
+    demo.queue().launch()
 
 if __name__ == "__main__":
     fire.Fire(main)
